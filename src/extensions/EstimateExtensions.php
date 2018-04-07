397db4d9a3e75f5ac043eb66845bb2c5d4203d0f
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-04-07 23:28:50 +0100

    Move shopping cart

--- src/extensions/EstimateExtensions.php
+++ src/extensions/EstimateExtensions.php
@@ -4,6 +4,7 @@ namespace SilverCommerce\ShoppingCart\Extensions;
 
 use SilverStripe\ORM\DataExtension;
 use SilverCommerce\ShoppingCart\Control\ShoppingCart;
+use SilverStripe\Forms\FieldList;
 
 /**
  * Overwrite group object so we can setup default groups
@@ -16,4 +17,17 @@ class EstimateExtension extends DataExtension
     private static $db = [
         "ShoppingCart" => "Boolean"
     ];
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $cart_field = $fields->dataFieldByName("ShoppingCart");
+        $fields->removeByName("ShoppingCart");
+        $sidebar = $fields->find("Name","OrdersSidebar");
+
+        foreach ($fields->flattenFields() as $field) {
+            if ($field->getName() == "OrdersSidebar") {
+                $field->push($cart_field);
+            }
+        }
+    }
 }
