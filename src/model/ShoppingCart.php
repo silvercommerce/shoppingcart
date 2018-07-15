676bdbe721fc1785b87a7ff05d37eba468c0d428
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-07-15 22:04:30 +0100

    Add link functions to shopping cart model class

--- src/model/ShoppingCart.php
+++ src/model/ShoppingCart.php
@@ -2,7 +2,11 @@
 
 namespace SilverCommerce\ShoppingCart\Model;
 
+use SilverStripe\Control\Director;
+use SilverStripe\Control\Controller;
+use SilverStripe\Core\Injector\Injector;
 use SilverCommerce\OrdersAdmin\Model\Estimate;
+use SilverCommerce\ShoppingCart\Control\ShoppingCart as ShoppingCartController;
 
 /**
  * Custom version of an estimate that is be mapped to the ShoppingCartController 
@@ -10,4 +14,42 @@ use SilverCommerce\OrdersAdmin\Model\Estimate;
 class ShoppingCart extends Estimate
 {
     private static $table_name = "ShoppingCart";
+
+        /**
+     * Get the link to this controller
+     *
+     * @param string $action The action you want to add to the link
+     * @return string
+     */
+    public function Link($action = null)
+    {
+        $controller = Injector::inst()->create(ShoppingCartController::class);
+        return $controller->Link($action);
+    }
+
+    /**
+     * Get an absolute link to this controller
+     *
+     * @param string $action The action you want to add to the link
+     * @return string
+     */
+    public function AbsoluteLink($action = null)
+    {
+        return Director::absoluteURL($this->Link($action));
+    }
+
+    /**
+     * Get a relative (to the root url of the site) link to this
+     * controller
+     *
+     * @param string $action The action you want to add to the link
+     * @return string
+     */
+    public function RelativeLink($action = null)
+    {
+        return Controller::join_links(
+            Director::baseURL(),
+            $this->Link($action)
+        );
+    }
 }
