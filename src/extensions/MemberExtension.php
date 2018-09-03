eac23256aaafcec693d187bf8b1c09d7b00b351e
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-09-03 18:06:58 +0100

    Add ability to get and set  the cart from a member

---
+++ src/extensions/MemberExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SilverCommerce\ShoppingCart\Extensions;
+
+use SilverStripe\ORM\DataExtension;
+use SilverCommerce\ShoppingCart\Model\ShoppingCart;
+
+/**
+ * Customise Member objects
+ * 
+ */
+class MemberExtension extends DataExtension
+{
+    /**
+     * Get the currenty active shopping cart on a member
+     * 
+     * @return ShoppingCart
+     */
+    public function getCart()
+    {
+        return $this
+            ->getOwner()
+            ->Contact()
+            ->Estimates()
+            ->find("ClassName", ShoppingCart::class);
+    }
+
+    /**
+     * Update the current cart. Also make sure no more than one is
+     * set at any one time.
+     * 
+     * @return self
+     */
+    public function setCart(ShoppingCart $cart)
+    {
+        $curr = $this->getOwner()->getCart();
+        $contact = $this->getOwner()->Contact();
+
+        if (isset($curr) && $curr->ID != $cart->ID) {
+            $curr->delete();
+        }
+
+        $cart->CustomerID = $contact->ID;
+
+        return $this;
+    }
+}
