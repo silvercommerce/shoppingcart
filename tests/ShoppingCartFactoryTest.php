dd27bd2dd0b85b3682af4efcc7bc0268dc7b28c3
Morven Lewis-Everley <morven@i-lateral.com>
2020-05-01 15:15:02 +0100

    Fix unit tests

--- tests/ShoppingCartFactoryTest.php
+++ tests/ShoppingCartFactoryTest.php
@@ -7,7 +7,6 @@ use SilverStripe\Control\Session;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Injector\Injector;
-use SilverCommerce\OrdersAdmin\Model\LineItem;
 use SilverCommerce\ShoppingCart\ShoppingCartFactory;
 use SilverCommerce\ShoppingCart\Tests\Model\TestProduct;
 
@@ -44,31 +43,37 @@ class ShoppingCartFactoryTest extends SapphireTest
 
     /**
      * Test setting up the cart using ShoppingCartFactory
-     * 
+     *
      * @return null
      */
     public function testConstruction()
     {
+        $use_cookies = ShoppingCartFactory::config()->use_cookies;
+        ShoppingCartFactory::config()->use_cookies = true;
         Cookie::set(ShoppingCartFactory::COOKIE_NAME, "abc123");
 
         $cart = ShoppingCartFactory::create()->getOrder();
 
         $this->assertEquals("abc123", $cart->AccessKey);
+
+        ShoppingCartFactory::config()->use_cookies = $use_cookies;
     }
 
     /**
      * Test adding an item to the shopping cart
-     * 
+     *
      * @return null
      */
     public function testAddItem()
     {
+        $use_cookies = ShoppingCartFactory::config()->use_cookies;
+        ShoppingCartFactory::config()->use_cookies = true;
         Cookie::set(ShoppingCartFactory::COOKIE_NAME, "abc123");
 
         $product = TestProduct::create(
             [
                 "Title" => "A stock item",
-                "PriceAmount" => 5.99
+                "BasePrice" => 5.99
             ]
         );
         $product->write();
@@ -80,21 +85,25 @@ class ShoppingCartFactoryTest extends SapphireTest
         $this->assertEquals(1, $cart->Items()->count());
         $this->assertEquals(2, $cart->TotalItems);
         $this->assertEquals(11.98, $cart->Total);
+
+        ShoppingCartFactory::config()->use_cookies = $use_cookies;
     }
 
     /**
      * Test updating an item increases the quantity and that
      * adding an existing item calls "update"
-     * 
+     *
      * @return null
      */
     public function testUpdateItem()
     {
+        $use_cookies = ShoppingCartFactory::config()->use_cookies;
+        ShoppingCartFactory::config()->use_cookies = true;
         Cookie::set(ShoppingCartFactory::COOKIE_NAME, "123abc");
 
         $cart = ShoppingCartFactory::create();
-        $item = $cart->getCurrent()->Items()->first();
-        $cart = $cart->updateItem($item->Key, 2)->getOrder();
+        $item = $cart->getOrder()->Items()->first();
+        $cart = $cart->updateItem($item->Key, 1)->getOrder();
 
         $this->assertEquals(1, $cart->Items()->count());
         $this->assertEquals(2, $cart->TotalItems);
@@ -104,7 +113,7 @@ class ShoppingCartFactoryTest extends SapphireTest
         $product = TestProduct::create(
             [
                 "Title" => "A cheap item",
-                "Price" => 5.99,
+                "BasePrice" => 5.99,
                 "Quantity" => 1,
                 "StockID" => "Item1"
             ]
@@ -118,15 +127,19 @@ class ShoppingCartFactoryTest extends SapphireTest
         $this->assertEquals(1, $cart->Items()->count());
         $this->assertEquals(3, $cart->TotalItems);
         $this->assertEquals(17.97, $cart->Total);
+
+        ShoppingCartFactory::config()->use_cookies = $use_cookies;
     }
 
     /**
      * Test removing an item
-     * 
+     *
      * @return null
      */
     public function testRemoveItem()
     {
+        $use_cookies = ShoppingCartFactory::config()->use_cookies;
+        ShoppingCartFactory::config()->use_cookies = true;
         Cookie::set(ShoppingCartFactory::COOKIE_NAME, "123abc");
 
         $cart = ShoppingCartFactory::create();
@@ -135,25 +148,31 @@ class ShoppingCartFactoryTest extends SapphireTest
 
         $this->assertEquals(0, $cart->Items()->count());
         $this->assertEquals(0, $cart->TotalItems);
+
+        ShoppingCartFactory::config()->use_cookies = $use_cookies;
     }
 
     /**
      * Test deleting the cart
-     * 
+     *
      * @return null
      */
     public function testDelete()
     {
+        $use_cookies = ShoppingCartFactory::config()->use_cookies;
+        ShoppingCartFactory::config()->use_cookies = true;
         Cookie::set(ShoppingCartFactory::COOKIE_NAME, "123abc");
 
         $cart = ShoppingCartFactory::create()->delete();
 
         $this->assertFalse($cart->getOrder()->exists());
+
+        ShoppingCartFactory::config()->use_cookies = $use_cookies;
     }
 
     /**
      * Test saving a new cart
-     * 
+     *
      * @return null
      */
     public function testWrite()
--- tests/model/TestProduct.php
+++ tests/model/TestProduct.php
@@ -2,15 +2,62 @@
 
 namespace SilverCommerce\ShoppingCart\Tests\Model;
 
+use SilverCommerce\TaxAdmin\Interfaces\TaxableProvider;
+use SilverCommerce\TaxAdmin\Model\TaxRate;
+use SilverCommerce\TaxAdmin\Traits\Taxable;
 use SilverStripe\Dev\TestOnly;
 use SilverStripe\ORM\DataObject;
 
-class TestProduct extends DataObject implements TestOnly
+class TestProduct extends DataObject implements TestOnly, TaxableProvider
 {
+    use Taxable;
+
     private static $db = [
         "Title" => "Varchar",
         "StockID" => "Varchar",
-        "Price" => "TaxableCurrency",
+        "BasePrice" => "Decimal(9,3)",
         "StockLevel" => "Int",
     ];
+
+    public function getBasePrice()
+    {
+        return $this->dbObject('BasePrice')->getValue();
+    }
+
+    public function getBulkPrice()
+    {
+        return $this->dbObject('BasePrice')->getValue();
+    }
+
+    public function getTaxRate()
+    {
+        return TaxRate::create(
+            [
+                "Title" => "VAT",
+                "Rate" => 20.00,
+                'Global' => true
+            ]
+        );
+    }
+
+    public function getLocale()
+    {
+        return 'en_GB';
+    }
+
+    public function getShowPriceWithTax()
+    {
+        return true;
+    }
+
+    public function getShowTaxString()
+    {
+        return false;
+    }
+
+    public function getPricingGroup()
+    {
+        // Slightly hacky way to get around BulkPricingModule (if installed)
+        return self::create(['ID' => -1]);
+    }
 }
